8ba08062b4fcb5b6feb0272e5f5228db59d1c2ac
Heber <hbrs_rootbattery@hotmail.com>
2023-07-13 12:34:42 -0300

    atualização 13-07-2023 - tipos de variaveis

--- variaveis.js
+++ variaveis.js
@@ -1,7 +1,29 @@
 // VARIÁVEIS
-testerildo
 
-let idade = 26
+let idade = 26 //Criando a primeira variavel
 
-console.log(idade)
 
+
+idade = 30            //number
+let numero = 30;      //number
+let altura = 1.77     //number
+let nome = "Heber"    //string
+let estudando = true  //booleano (true ou false)
+
+console.log(idade, typeof idade)
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra;
+console.log(outra)
+
+let nula = null;
+console.log(nula)
+
+var variavel = 30;  // Não utilizar var
+console.log(variavel)
+
+const constante = 100;
+constante = 20;
