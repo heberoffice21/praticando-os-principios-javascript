a812f932a803d5c5c049a71f8c73a7ddf2ec1f64
Heber <hbrs_rootbattery@hotmail.com>
2023-07-13 13:17:57 -0300

    atualização 13-07-2023 -13:17-operadores aritméticos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,22 @@
+// OPERADORES ARITMÉTICOS
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+
+
+//Adição:                   +
+//Subtração:                -
+//Multiplicação:            *
+//Divisão:                  /
+//Exponenciação:            **
+//Resto de divisão:         %
+
+const soma = primeiroNumero + segundoNumero
+
+console.log('Soma =', soma)
+console.log('Subtração =', primeiroNumero - segundoNumero)
+console.log('Multiplicação =', primeiroNumero * segundoNumero)
+console.log('Divisão =', primeiroNumero / segundoNumero)
+console.log('Exponenciação =', primeiroNumero ** segundoNumero)
+console.log('Resto de divisão =', primeiroNumero % segundoNumero)
