bfc6a1f88d567611838f81932d9e6252acccb0b2
Heber <hbrs_rootbattery@hotmail.com>
2023-07-14 08:22:03 -0300

    atualização 14-07-2023-08:21- operadores booleanos p1

---
+++ operadores_booleanos.js
@@ -0,0 +1,27 @@
+// OPERADORES BOOLEANOS
+
+// Igualdade: == (ou ===)
+//Desiqualdade: != (ou !==)
+//Maior que: >
+//Maior ou igual: >=
+//Menor que: <
+// Menor ou igual <=
+
+const numero = 10
+
+console.log(numero > 20)
+console.log(numero == 10)
+
+console.log(numero == "10")
+console.log(10 == "10")
+console.log(10 === "10")
+
+console.clear()
+
+console.log(10 != 10)
+console.log(10 !== "10")
+
+
+
+
+
