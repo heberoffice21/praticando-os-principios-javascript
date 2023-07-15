42aeaeeb7da75aacf2d014da63acf6abe1aaa836
Heber <hbrs_rootbattery@hotmail.com>
2023-07-14 21:12:29 -0300

    atualização 14-07-2023 - 21:11 - finalizando a pratica usando operadores booleanos: and, or e not

--- operadores_booleanos.js
+++ operadores_booleanos.js
@@ -21,6 +21,30 @@ console.clear()
 console.log(10 != 10)
 console.log(10 !== "10")
 
+//Conjunções Lógicas
+
+//And => &&
+
+let idade = 10;
+let tenhoCnh = false;
+
+const possoDirigir = idade >= 18 && tenhoCnh === true
+
+console.log("posso dirijir?", possoDirigir)
+
+
+// Or => ||
+
+idade = 70
+
+const votoFacultativo = idade < 18 || idade >= 70
+
+console.log("posso votar?", votoFacultativo)
+
+// Not => !
+
+const estouGostandoDestaPratica = true
+console.log(!estouGostandoDestaPratica)
 
 
 
