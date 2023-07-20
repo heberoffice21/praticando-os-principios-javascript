1d41ae675b751d048a1119c0eba06f8b7d2bf56f
Heber <hbrs_rootbattery@hotmail.com>
2023-07-20 15:10:17 -0300

    20-07-2023 - laçoes condicionais - sistema de repetição

---
+++ laços_condicionais.js
@@ -0,0 +1,14 @@
+// > Laços Condicionais
+
+const input = require("readline-sync");
+
+const numero_sorteado = 5;
+
+let numero = Number (input.question("Qual numero voce escolhe? ") );
+
+while (numero !== numero_sorteado) {
+    console.log("Você errou o número. Tente novamente...")
+    numero = Number (input.question("Qual numero voce escolhe? "))
+}
+
+ console.log("Voce acertou")
