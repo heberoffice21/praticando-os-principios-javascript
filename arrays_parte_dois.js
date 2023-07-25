cb0cc343b917d8bbe473f1428ac9a050aa89de9b
Heber <hbrs_rootbattery@hotmail.com>
2023-07-25 08:18:13 -0300

    atualização 25-07-2023 mostrei alguns metodos/funções de arrays mostrei como utilizar o splice e o push

---
+++ arrays_parte_dois.js
@@ -0,0 +1,19 @@
+// > ARRAYS - Parte  - MÉTODOS DE ARRAYS
+
+let arr1 = [30, 12, 45, 34, 29]
+let arr2 = []
+
+// FATIAMENTO: SLICE
+
+console.log(arr1.splice(2))
+
+console.clear()
+
+// Adicionando elementos : push |unshift
+
+console.log('Antes de adicionar elemento', arr2)
+
+arr2.push(10, 20, 30)
+
+console.log('Depois de adicionar elemento', arr2)
+
