406b6ef9202978215ce13320a6fc932c1a0de972
Heber <hbrs_rootbattery@hotmail.com>
2023-08-08 07:43:50 -0300

    copia segurança

---
+++ cópia de segurança/variaveis.js
@@ -0,0 +1,29 @@
+// VARIÁVEIS
+
+let idade = 26 //Criando a primeira variavel
+
+
+
+idade = 30            //number
+let numero = 30;      //number
+let altura = 1.77     //number
+let nome = "Heber"    //string
+let estudando = true  //booleano (true ou false)
+
+console.log(idade, typeof idade)
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra;
+console.log(outra)
+
+let nula = null;
+console.log(nula)
+
+var variavel = 30;  // Não utilizar var
+console.log(variavel)
+
+const constante = 100;
+constante = 20;
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Exemplo da integração do javascript com o HTML")
+console.log("Outra linha")
